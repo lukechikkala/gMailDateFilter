a156c54d36f71c2f89a11440f53999d55df051c0
lukechikkala <lukechikkala@gmail.com>
2026-06-30 00:00:21 +0200

    Added dummy year and working date parser

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <chrono>
 #include <cstdio>
 #include <ctime>
+#include <exception>
 #include <iostream>
 #include <optional>
 #include <regex>
@@ -16,6 +17,53 @@ struct Date
     int Year;
 };
 
+int Current_Year()
+{
+    // for now return raw value.
+    return 2026;
+}
+
+bool Is_Leap( int year )
+{
+    return ( year % 4 == 0 && year % 100 != 0 ) || ( year % 400 == 0 );
+}
+
+Date Parse_And_Validate( const std::string& input )
+{
+    // Should accept DD.MM where YYYY is taken as the current Year.
+    // or DD.MM.YYYY where the user specifically be able to give the year.
+
+    static const std::regex pattern( R"((\d{1,2})\.(\d{1,2})(?:\.(\d{4}))?)" );
+    std::smatch match;
+
+    if ( !std::regex_match( input, match, pattern ) )
+        throw std::runtime_error( "Invalid Input: Expected DD.MM or DD.MM.YYYY" );
+
+    const int day   = std::stoi( match[ 1 ]);
+    const int month = std::stoi( match[ 2 ]);
+          int year  = Current_Year();
+    if ( match[ 3 ].matched )
+        year = std::stoi( match[ 3 ] );
+
+    if ( month < 1 || month > 12 )
+        throw std::runtime_error( "Invalid Month: " + std::to_string( month ) );
+
+    static constexpr int days_in_month[] = {
+          31, 28, 31, 30, 31, 30, 31
+        , 31, 30, 31, 30, 31
+    };
+    int day_max = days_in_month[ month - 1 ];
+    if ( month == 2 && Is_Leap( year ) )
+        day_max = 29;
+
+    if ( day < 1 || day > day_max )
+        throw std::runtime_error( "Invald Day: " + std::to_string( day   )
+                                + " for month: " + std::to_string( month )
+                );
+
+    return { day, month, year };
+}
+
 std::optional<Date> Read_Date( const std::string& prompt )
 {
     std::cout << prompt;
@@ -26,9 +74,16 @@ std::optional<Date> Read_Date( const std::string& prompt )
         return std::nullopt;
     }
 
-    std::cout << input << std::endl;
+    try
+    {
+        return Parse_And_Validate( input );
+    }
+    catch ( const std::exception& e )
+    {
+        std::cout << e.what() << '\n';
+        return std::nullopt;
+    }
 
-    return std::nullopt;
 }
 
 void Date_Filter()
